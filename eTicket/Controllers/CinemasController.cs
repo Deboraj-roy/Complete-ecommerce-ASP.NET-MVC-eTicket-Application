781d3ac15f910c0c56d38e10074539b81f0b7b33
deboraj-roy <deborajroy123@gmail.com>
2023-12-06 02:40:26 +0600

    Cinemas Controller

---
+++ eTicket/Controllers/CinemasController.cs
@@ -0,0 +1,22 @@
+﻿using eTicket.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTicket.Controllers
+{
+    public class CinemasController : Controller
+    {
+
+        private readonly AppDbContext _context;
+        public CinemasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async IActionResult Index()
+        {
+            var allCinemas = await _context.Cinemas.ToListAsync();
+            return View();
+        }
+    }
+}
---
+++ eTicket/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using eTicket.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTicket.Controllers
+{
+    public class MoviesController : Controller
+    {
+
+        private readonly AppDbContext _context;
+        public MoviesController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> IndexAsync()
+        {
+
+            var allProducers = await _context.Movies.ToListAsync();
+            return View();
+        }
+    }
+}
