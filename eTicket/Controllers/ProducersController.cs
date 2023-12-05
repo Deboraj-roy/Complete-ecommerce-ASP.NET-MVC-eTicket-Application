d047209c0405bd57251887e28a66ee4ef505a351
deboraj-roy <deborajroy123@gmail.com>
2023-12-06 02:37:10 +0600

    Adding Producers Controller

---
+++ eTicket/Controllers/ProducersController.cs
@@ -0,0 +1,21 @@
+﻿using eTicket.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTicket.Controllers
+{
+    public class ProducersController : Controller
+    {
+
+        private readonly AppDbContext _context;
+        public ProducersController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async IActionResult Index()
+        {
+            var allProducers = await _context.Producers.ToListAsync();
+            return View();
+        }
+    }
+}
