e67244598dcfd6ca5e3cf7c741672ed77fd5c3ea
deboraj-roy <deborajroy123@gmail.com>
2023-12-06 17:22:14 +0600

    27. Designing the movie card

--- eTicket/Controllers/MoviesController.cs
+++ eTicket/Controllers/MoviesController.cs
@@ -15,7 +15,7 @@ namespace eTicket.Controllers
         public async Task<IActionResult> Index()
         {
 
-            var allMovies = await _context.Movies.ToListAsync();
+            var allMovies = await _context.Movies.Include(n => n.Cinema).OrderBy(n => n.Name).ToListAsync();
             return View(allMovies);
         }
     }
