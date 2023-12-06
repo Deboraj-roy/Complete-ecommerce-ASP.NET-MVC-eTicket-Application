679b75c87d1a2d5d4e9649ffac5e03fa303deee6
deboraj-roy <deborajroy123@gmail.com>
2023-12-06 17:02:01 +0600

    26. Adding the movies view

--- eTicket/Controllers/MoviesController.cs
+++ eTicket/Controllers/MoviesController.cs
@@ -12,11 +12,11 @@ namespace eTicket.Controllers
         {
             _context = context;
         }
-        public async Task<IActionResult> IndexAsync()
+        public async Task<IActionResult> Index()
         {
 
-            var allProducers = await _context.Movies.ToListAsync();
-            return View();
+            var allMovies = await _context.Movies.ToListAsync();
+            return View(allMovies);
         }
     }
 }
