86bbb802346a30c6e1f6a31bd8bf91e2fddf84c6
deboraj-roy <deborajroy123@gmail.com>
2023-12-07 18:36:55 +0600

    32. Designing your first form | Create Actor

--- eTicket/Controllers/ActorsController.cs
+++ eTicket/Controllers/ActorsController.cs
@@ -16,5 +16,12 @@ namespace eTicket.Controllers
             var data = await _service.GetAll();
             return View(data);
         }
+
+        //Get: Actors/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+         
     }
 }
