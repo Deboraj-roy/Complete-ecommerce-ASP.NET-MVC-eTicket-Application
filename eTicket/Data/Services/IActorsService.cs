ba6acc5f63bddcf1b7c01db4082ec7f53d0b064a
deboraj-roy <deborajroy123@gmail.com>
2023-12-07 17:39:56 +0600

    29. Adding IActorsService interface

---
+++ eTicket/Data/Services/IActorsService.cs
@@ -0,0 +1,13 @@
+﻿using eTicket.Models;
+
+namespace eTicket.Data.Services
+{
+    public interface IActorsService
+    {
+        IEnumerable<Actor> GetAll();
+        Actor GetById(int id);  
+        void Add(Actor actor);
+        Actor Update(int id, Actor newActor);
+        void Delete(int id);
+    }
+}
--- eTicket/Program.cs
+++ eTicket/Program.cs
@@ -10,13 +10,6 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(buil
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
