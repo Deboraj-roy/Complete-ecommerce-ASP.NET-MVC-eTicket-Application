76a9aaf7f690005b499b4eee000c9ad41a2ab38b
deboraj-roy <deborajroy123@gmail.com>
2023-12-07 20:46:28 +0600

    34. Getting item by id (Actor Details)

--- eTicket/Controllers/ActorsController.cs
+++ eTicket/Controllers/ActorsController.cs
@@ -14,7 +14,7 @@ namespace eTicket.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            var data = await _service.GetAll();
+            var data = await _service.GetAllAsync();
             return View(data);
         }
 
@@ -31,8 +31,19 @@ namespace eTicket.Controllers
             {
                 return View(actor);
             }
-            _service.Add(actor);
+             await _service.AddAsync(actor);
             return RedirectToAction(nameof(Index));
+        }
+
+        //Get: Actor/Details/1
+        public async Task<IActionResult> Details(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+            if (actorDetails == null)
+            {
+                return View("Empty");
+            }
+            return View(actorDetails);
         }
 
     }
--- eTicket/Data/Services/ActorsService.cs
+++ eTicket/Data/Services/ActorsService.cs
@@ -11,10 +11,10 @@ namespace eTicket.Data.Services
         {
             _context = context;
         }
-        public void Add(Actor actor)
+        public async Task AddAsync(Actor actor)
         {
-            _context.Actors.Add(actor);
-            _context.SaveChanges();
+            await _context.Actors.AddAsync(actor);
+            await _context.SaveChangesAsync();
         }
 
         public void Delete(int id)
@@ -22,15 +22,16 @@ namespace eTicket.Data.Services
             throw new NotImplementedException();
         }
 
-        public async Task<IEnumerable<Actor>> GetAll()
+        public async Task<IEnumerable<Actor>> GetAllAsync()
         {
             var result = await _context.Actors.ToListAsync();
             return result;
         }
 
-        public Actor GetById(int id)
+        public async Task<Actor> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            var result = await _context.Actors.FirstOrDefaultAsync(x => x.Id == id);
+            return result;
         }
 
         public Actor Update(int id, Actor newActor)
