9df1bf3bfd75f90a6e04f596950d48dca327d191
deboraj-roy <deborajroy123@gmail.com>
2023-12-11 16:22:31 +0600

    44. Getting producer details

--- eTicket/Controllers/ProducersController.cs
+++ eTicket/Controllers/ProducersController.cs
@@ -18,5 +18,12 @@ namespace eTicket.Controllers
             var allProducers = await _service.GetAllAsync();
             return View(allProducers);
         }
+        //Get: Producers/Details/1
+        public async Task<IActionResult> Details(int id)
+        {
+            var producerDetails = await _service.GetByIdAsync(id);
+            if (producerDetails == null) return View("NotFound");
+            return View(producerDetails);
+        }
     }
 }
