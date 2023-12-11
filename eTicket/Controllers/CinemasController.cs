7a7bb076e254ecd3f00037cff40bccf63d5d49b7
deboraj-roy <deborajroy123@gmail.com>
2023-12-12 02:10:50 +0600

    Section 11: Working with cinema data, 52. Deleting a Cinema from database

--- eTicket/Controllers/CinemasController.cs
+++ eTicket/Controllers/CinemasController.cs
@@ -45,5 +45,39 @@ namespace eTicket.Controllers
             if (cinemaDetails == null) return View("NotFound");
             return View(cinemaDetails);
         }
+
+        //Get: Cinemas/Edit/1
+        public async Task<IActionResult> Edit(int id)
+        {
+            var cinemaDetails = await _service.GetByIdAsync(id);
+            if (cinemaDetails == null) return View("NotFound");
+            return View(cinemaDetails);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Logo,Name,Description")] Cinema cinema)
+        {
+            if (!ModelState.IsValid) return View(cinema);
+            await _service.UpdateAsync(id, cinema);
+            return RedirectToAction(nameof(Index));
+        }
+
+        //Get: Cinemas/Delete/1
+        public async Task<IActionResult> Delete(int id)
+        {
+            var cinemaDetails = await _service.GetByIdAsync(id);
+            if (cinemaDetails == null) return View("NotFound");
+            return View(cinemaDetails);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirm(int id)
+        {
+            var cinemaDetails = await _service.GetByIdAsync(id);
+            if (cinemaDetails == null) return View("NotFound");
+
+            await _service.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
