b32fcf04d2a79387c4d2bfd1e4110784bf31dd45
deboraj-roy <deborajroy123@gmail.com>
2023-12-19 19:00:50 +0600

    72. Removing items from shopping cart

--- eTicket/Controllers/OrdersController.cs
+++ eTicket/Controllers/OrdersController.cs
@@ -29,13 +29,24 @@ namespace eTicket.Controllers
             return View(response);
         }
 
-        public async Task<RedirectToActionResult> AddItemToShoppingCart(int id)
+        public async Task<IActionResult> AddItemToShoppingCart(int id)
         {
             var item = await _moviesService.GetMovieByIdAsync(id);
             if (item != null)
             {
                 _shoppingCart.AddItemToCart(item);
-            } 
+            }
+            return RedirectToAction(nameof(ShoppingCart));
+
+        }
+
+        public async Task<IActionResult> RemoveItemFromShoppingCart(int id)
+        {
+            var item = await _moviesService.GetMovieByIdAsync(id);
+            if (item != null)
+            {
+                _shoppingCart.RemoveItemFromCart(item);
+            }
             return RedirectToAction(nameof(ShoppingCart));
 
         }
