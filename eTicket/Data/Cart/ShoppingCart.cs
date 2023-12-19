2010e9a48878063d3a0e804281e2cc2d10708f66
deboraj-roy <deborajroy123@gmail.com>
2023-12-19 16:26:30 +0600

    65. Setting up ShoppingCart class

---
+++ eTicket/Data/Cart/ShoppingCart.cs
@@ -0,0 +1,27 @@
+﻿using eTicket.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTicket.Data.Cart
+{
+    public class ShoppingCart
+    {
+        private readonly AppDbContext _context;
+
+        public string ShoppingCartId { get; set; }
+        public List<ShoppingCartItem> shoppingCartItems { get; set; }
+
+        public ShoppingCart(AppDbContext context)
+        { 
+            _context = context;
+        }
+
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            return shoppingCartItems ?? (shoppingCartItems = _context.ShoppingCartItems
+                .Where(n => n.ShoppingCartId == ShoppingCartId).Include(n => n.Movie).ToList());
+        }
+
+        public double GetShoppingCartTotal() => _context.ShoppingCartItems.Where(n => n.ShoppingCartId == ShoppingCartId)
+            .Select(n => n.Movie.Price * n.Amount).Sum();
+    }
+}
