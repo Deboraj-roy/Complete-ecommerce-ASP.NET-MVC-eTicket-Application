0335a632cca981048259be63734a1ab31f9c14df
deboraj-roy <deborajroy123@gmail.com>
2023-12-20 14:13:50 +0600

    73. ViewComponents

---
+++ eTicket/Data/ViewComponents/ShoppingCartSummary.cs
@@ -0,0 +1,20 @@
+﻿using eTicket.Data.Cart;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTicket.Data.ViewComponents
+{
+    public class ShoppingCartSummary : ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            return View(items.Count);
+        }
+    }
+}
