4ea989913eb88ede3490df447652ea3f84cf753c
deboraj-roy <deborajroy123@gmail.com>
2023-12-27 18:15:08 +0600

    88. Logging in users

--- eTicket/Controllers/AccountController.cs
+++ eTicket/Controllers/AccountController.cs
@@ -15,18 +15,49 @@ namespace eTicket.Controllers
 
         public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager,
             AppDbContext context, ILogger<AccountController> logger)
-        { 
+        {
             _userManager = userManager;
             _signInManager = signInManager;
             _context = context;
             _logger = logger;
-        }  
+        }
 
         public IActionResult Login()
         {
             _logger.LogInformation("I am in Account Controller.");
 
             return View(new LoginVM());
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        {
+            if (!ModelState.IsValid) return View(loginVM);
+
+            var user = await _userManager.FindByEmailAsync(loginVM.EmailAddress);
+            if (user != null)
+            {
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, loginVM.Password);
+                if (passwordCheck)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        _logger.LogError("Logged in Success.");
+                        return RedirectToAction("Index", "Movies");
+                    }
+                }
+
+                _logger.LogError("Wrong credentials. Please, try again!");
+                TempData["LoinError"] = "Wrong credentials. Please, try again!";
+                return View(loginVM);
+
+            }
+
+            _logger.LogError("Wrong credentials. Please, try again!");
+            TempData["LoinError"] = "Wrong credentials. Please, try again!";
+            return View(loginVM);
+
         }
     }
 }
