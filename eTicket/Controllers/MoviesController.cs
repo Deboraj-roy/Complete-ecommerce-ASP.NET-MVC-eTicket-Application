698848871af8810b85949f7aa21ec8b9a405fc7e
deboraj-roy <deborajroy123@gmail.com>
2024-01-17 00:59:22 +0600

    105. Ignore search string case : improve search

--- eTicket/Controllers/MoviesController.cs
+++ eTicket/Controllers/MoviesController.cs
@@ -35,8 +35,10 @@ namespace eTicket.Controllers
             var allMovies = await _service.GetAllAsync(n => n.Cinema);
             if (!string.IsNullOrEmpty(searchString))
             {
-                var filteredResult = allMovies.Where(n => n.Name.Contains(searchString)
-                || n.Description.Contains(searchString)).ToList();
+                //var filteredResult = allMovies.Where(n => n.Name.ToLower().Contains(searchString.ToLower()) || n.Description.ToLower().Contains(searchString.ToLower())).ToList();
+
+                var filteredResult = allMovies.Where(n =>string.Equals(n.Name, searchString, StringComparison.CurrentCultureIgnoreCase) ||
+                string.Equals(n.Description, searchString, StringComparison.CurrentCultureIgnoreCase)).ToList();
 
                 if (filteredResult.Count == 0)
                 {
